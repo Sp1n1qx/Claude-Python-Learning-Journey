c5553bb71301c4125cff7cdb255d4ab1b85d0f1a
Sp1n1qx <255153518+Sp1n1qx@users.noreply.github.com>
2026-06-07 21:19:16 +0100

    change num02

--- 1y_project.py
+++ 1y_project.py
@@ -1,4 +1,4 @@
-# suspence
+# time of suspence
 import time as t
 
 # make a escape/thinking game for my wife
@@ -8,32 +8,23 @@ score = 0
 # name of the game
 print("Escape and Think...\n")
 t.sleep(0.3)
-# print and then input on each question
 
 def show():
-    print("Lvl 1")
-    t.sleep(0.3)
-    print("Lvl 2")
-    t.sleep(0.3)
-    print("Lvl 3")
-    t.sleep(0.3)
-    print("Lvl 4")
-    t.sleep(0.3)
-    print("Lvl 5\n")
+    for x in range(5):
+        print("lvl",x + 1)
+        t.sleep(0.3)
 
-while True:
-    show()
-    choice = input("choose your lvl: ")
-    if choice == "1":
-        print("To open the safe, enter 4 digit code.\n" \
-        "hint: the month we met + the year we are * 16*14 / 30.")
-        answer = int(input())
-        if answer == 2034:
-            score = score + 1
-            print("Good job.")
-            print(f"this is your score: {score}")
-        else:
-            print("Try again.")
-            print(f"this is your score: {score}")
+def lvl1():
+    print("To open the safe, enter 4 digit code.\n" \
+    "hint: the month we met + the year we are * 16*14 / 30.")
+    answer = int(input())
+    if answer == 2034:
+        print("Good job.")
+    else:
+        print("Try again.")
 
+show()
+choice = input("choose your lvl: ")
+if choice == "1":
+    lvl1()
 #needs a "if see answers print answers, if not try again" type of thing, so she would decide if she wants or no
