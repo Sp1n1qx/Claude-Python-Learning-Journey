f71856a930e3d55797c4860142b59111814287cc
Sp1n1qx <255153518+Sp1n1qx@users.noreply.github.com>
2026-06-07 17:54:46 +0100

    added exercise 1y_project.py

---
+++ 1y_project.py
@@ -0,0 +1,7 @@
+# make a escape/thinking game for my wife
+
+# name of the game
+
+# print and then input on each question
+
+#needs a "if see answers print answers, if not try again" type of thing, so she would decide if she wants or no
