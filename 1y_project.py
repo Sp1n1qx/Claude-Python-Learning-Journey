2bfc893ce35671bcfa4b8b12fad8959a8b90e85e
Sp1n1qx <255153518+Sp1n1qx@users.noreply.github.com>
2026-06-09 09:59:11 +0100

    added more code but i will need to change the hints to a dictionary

--- 1y_project.py
+++ 1y_project.py
@@ -1,5 +1,7 @@
 # time of suspence
 import time as t
+# cleaning spaces when much much has been writen
+import os as clean
 
 # make a escape/thinking game for my wife
 # score
@@ -7,22 +9,27 @@ score: int = 0
 
 # name of the game
 print("Escape and Think...\n")
+# wait time
 t.sleep(0.3)
 
+# function to show all levels available
 def show():
     for x in range(5):
         print("lvl", x + 1)
         t.sleep(0.3)
 
+# lvl 1
 def lvl1():
     print("To open the safe, enter 4 digit first last code.\n" \
     "hint: the month we met / the year we are * 16*14 * 30.")
+    # while doesnt get right, try to answer
     while True:
         answer = input()
         if answer == "2398":
-            print("Good job.")
+            print("Good job darling!")
             return 1
         else:
+            # if not right show prompt and to try again 
             print("Try again.\n")
             print("To open the safe, enter 4 digit code.\n" \
             "hint: the month we met + the year we are * 16*14 / 30.")
@@ -38,9 +45,17 @@ def lvl2():
             return 1
         elif answer == "help":
             answer = input("choose your hint, 1-3\n"
-            "write like hint 1/etc...")
+            "write like hint 1/etc... ")
             if answer == "hint 1":
-                print("This is like the videos on tik tok you see someone putting letters as a meaning.")
+                print("This is like the videos on tik tok you see someone putting letters as a meaning.\n" \
+                "IWTMY")
+                answer = input("Do you want the other hint for now or no? ")
+                if answer == "no":
+                    print("IWTMY")
+                elif answer == "yes":
+                    print("You are in our house, and you forgot the code, what is the code?\n" \
+                    "hint: IWTMY\n" \
+                    "each Letter = one word")
             elif answer == "hint 2":
                 print("You are in our house, and you forgot the code, what is the code?\n" \
                 "hint: IWTMY\n" \
@@ -57,4 +72,5 @@ elif choice == "2":
 #needs a "if see answers print answers, if not try again" type of thing, so she would decide if she wants or no
 
 
+#need to use dictionary for hints
 # add "import os" to use as os.system("clear"), and comment more on the code.
