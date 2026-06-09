8016412a80a9bddef801f10fb6ef9f56fa0964cd
Sp1n1qx <255153518+Sp1n1qx@users.noreply.github.com>
2026-06-09 15:57:53 +0100

    added lvl3, exceptions and bug fixes

--- 1y_project.py
+++ 1y_project.py
@@ -17,7 +17,7 @@ lvl2_hint = {
 # maybe hints for lvl 3? idk yet
 
 # name of the game
-print("Escape and Think...\n")
+print("Escape and Think...")
 # wait time
 t.sleep(0.3)
 
@@ -33,11 +33,12 @@ def show():
 
 # lvl 1
 def lvl1():
+    clear()
     print("To open the safe, enter 4 digit first last code.\n" \
     "hint: the month we met / the year we are * 16*14 * 30.")
     # while doesnt get right, try to answer again
     while True:
-        answer = input()
+        answer = input("Answer Here: ")
         if answer == "2398":
             print("Good job darling!")
             return 1
@@ -49,39 +50,77 @@ def lvl1():
             "hint: the month we met + the year we are * 16*14 / 30.")
 
 def lvl2():
+    clear()
     print("You are in our house, and you forgot the code, what is the code?\n" \
     "hint: IWTMY\n" \
     "to get hints write .help.")
     while True:
-        answer = input()
+        answer = input("Say Answer/Get Help here: ")
         if answer.upper() == "I WANT TO MARRY YOU":
             print("Good Girl.")
             return 1
         elif answer == "help":
             clear()
             answer = input("choose your hint, 1-3\n"
-            "write like hint 1/etc... ")
-        if answer == "hint 1":
+            "write like hint 1/etc... \n")
+        elif answer == "hint 1":
+            clear()
             print(lvl2_hint["hint 1"])
             print()
             answer = input("Would you like to see the next hint? (yes/no) ")
             if answer.lower() == "no":
-                print("Answer: ")
+                print()
             else:
                 print(lvl2_hint["hint 2"])
+                print("Answer:")
+        elif answer == "hint 2":
+            clear()
+            print(lvl2_hint["hint 2"])
+            print()
+        else:
+            clear()
+            print("Try again")
+
+def lvl3():
+    clear()
+    print("You need to find a math calculation to reach 08072025, how will you do?\n" \
+    "hint: you only need this numbers;\n" \
+    "2,3,5,7,271\n")
+    while True:
+        answer = input("Answer: ")
+        if answer == "3*3 5*5 7*7 * 271":
+            print("Correct!")
+        else:
+            choice = input("Do you want a hint? ")
+            if choice.lower() == "yes":
+                clear()
+                print("You need to find a math calculation to reach 08072025, how will you do?\n" \
+                "hint: you only need this numbers;\n" \
+                "2,3,5,7,271\n")
+                print("you can use those numbers to add, subract, divide and multiply\n" \
+                "but doesnt mean you need to use all of those.")
+            elif choice.lower() == "no":
+                clear()
+                print("You need to find a math calculation to reach 08072025, how will you do?\n" \
+                "hint: you only need this numbers;\n" \
+                "2,3,5,7,271\n")
 
 show()
 
-choice = input("choose your lvl: ")
-if choice == "1":
-    lvl1()
-elif choice == "2":
-    lvl2()
-
-# needs a "if see answers print answers, if not try again" type of thing, so she would decide if she wants or no
-
-
-# need to use dictionary for hints, need function to clear
-# add "import os" to use as os.system("clear"), and comment more on the code.
+while True:
+    try:
+        choice = int(input("choose your lvl (1-5): "))
+        if choice == 1:
+            lvl1()
+        elif choice == 2:
+            lvl2()
+        elif choice == 3:
+            lvl3()
+    except ValueError:
+        clear()
+        print("Invalid Output!")
+        print("Try again.\n")
 
 # look discord for one inspiration
+
+# for you to be able to find the answer i would sugest you to only use the multiply
